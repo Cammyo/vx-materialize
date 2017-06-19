b8300472d9dd402fdbb379398c980ac05d0f281e
Cameron Makowski <cameronmakowski@gmail.com>
2017-06-19 15:57:40 -0600

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,6 @@ $('.button-collapse').sideNav({
 
 
 $(document).ready(function() {
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  //button data-collapse
   $('.modal').modal();
 });
